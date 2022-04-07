7166b7852d8cc0b62fc1249a01289173b865a225
Mio <MioDevelops@gmail.com>
2022-04-07 10:43:03 -0400

    changes

--- Application.py
+++ Application.py
@@ -12,6 +12,7 @@ class Screen():
         self.mainbgtext = "#1ecbe1"
         self.mainbgbutton = "#1695a5"
         self.title = Label(self.root, text="Python File Comparer", font=("Helvetica", 20), bg=self.mainbgtext)
+        self.title_place = (110, 10)
         self.title.place(x=110, y=10)
 
     def callback(self, url):
@@ -28,14 +29,18 @@ class Screen():
     def main(self):
         self.root.update()
 
-        start = Button(self.root, text="Start Compairing", command=self.working(), bg=self.mainbgbutton, font=("Helvetica", 11, ("bold", "italic")), width=15)
+        start = Button(self.root, text="Start Compairing", command=lambda : self.working(), bg=self.mainbgbutton, font=("Helvetica", 11, ("bold", "italic")), width=15)
         start.place(x=170, y=150)
       
-        settings = Button(self.root, text="Settings", command=self.settings(), bg=self.mainbgbutton, font=("Helvetica", 11, ("bold", "italic")), width=15)
+        settings = Button(self.root, text="Settings", command=lambda : self.settings(), bg=self.mainbgbutton, font=("Helvetica", 11, ("bold", "italic")), width=15)
         settings.place(x=170, y=210)
 
-        credits = Button(self.root, text="Credits", command=self.results(), bg=self.mainbgbutton, font=("Helvetica", 11, ("bold", "italic")), width=15)
+        credits = Button(self.root, text="Credits", command=lambda : self.results(), bg=self.mainbgbutton, font=("Helvetica", 11, ("bold", "italic")), width=15)
         credits.place(x=170, y=270)
+
+        credits = Label(self.root, text="Created by: Conner Lovely", font=("Helvetica", 10, "bold"), cursor="hand2", bg=self.mainbgtext)
+        credits.place(x=325, y=475)
+        credits.bind("<Button-1>", lambda e: self.callback("https://github.com/MioDevelops"))
 
     def settings(self):
         self.minifest()
@@ -54,8 +59,14 @@ class Screen():
         pass
 
     def minifest(self):
-        #update and remoev widgets
+        #update and remove widgets
         self.root.update()
+
+        x = self.root.place_slaves()
+        for i in x:
+            i.place_forget()
+
+        self.title.place(x=self.title_place[0], y=self.title_place[1])
         pass
 
 screen = Screen()
